04ab4bc76e8bc8293cd6fde895df1c9f9bbc5e88
Cang Le <cangle1710@gmail.com>
2020-12-15 20:03:47 -0600

    adding enums and JSON message construction for subscribing

--- src/main/java/com/gsr/marketdata/MainRunner.java
+++ src/main/java/com/gsr/marketdata/MainRunner.java
@@ -1,12 +1,19 @@
 package com.gsr.marketdata;
-import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
 
 public class MainRunner {
     private static final Logger logger = Logger.getLogger(MainRunner.class.getName());
-    private static CountDownLatch latch;
-    public static void main(String[] args) throws InterruptedException {
-        OrderBookService orderBookService = new OrderBookService();
+
+    public static void main(String[] args) {
+        String instrument = "";
+        if(args.length != 1){
+            logger.warning("Invalid input. Expecting 1 instrument to subscribe to for market data");
+            System.exit(0);
+        }
+        else{
+            instrument = args[0];
+        }
+        OrderBookService orderBookService = new OrderBookService(instrument);
         orderBookService.doEnable();
 
 //        Thread.sleep(2000);
--- src/main/java/com/gsr/utils/OrderBookUtils.java
+++ src/main/java/com/gsr/utils/OrderBookUtils.java
@@ -1,5 +1,68 @@
 package com.gsr.utils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class OrderBookUtils {
     public static final String COIN_BASE_WEBSOCKET = "wss://ws-feed.pro.coinbase.com";
+    public static final String TYPE = "type";
+    public static final String PRODUCT_IDS = "product_ids";
+    public static final String CHANNELS = "channels";
+
+    public enum Type{
+        SUBSCRIBE("subscribe"),
+        UNSUBSCRIBE("unsubscribe");
+
+        private String type;
+        Type(String type){
+            this.type = type;
+        }
+
+        public String getType(){
+            return type;
+        }
+    }
+
+    public enum Channel{
+        L2("level2");
+
+        private String channel;
+        Channel(String channel){
+            this.channel = channel;
+        }
+
+        public String getChannel(){
+            return channel;
+        }
+    }
+    /**
+     * Example subscribe message
+     * {
+     *     "type": "subscribe",
+     *     "product_ids": [
+     *         "BTC-USD"
+     *     ],
+     *     "channels": [
+     *         "level2"
+     *     ]
+     * }
+     *
+     * @return
+     */
+    public static String constructLevel2JsonMessageWithType( Type type, String productId, Channel channel){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(TYPE, type.getType());
+
+        // product ids json array
+        JSONArray productIds = new JSONArray();
+        productIds.add(productId);
+        jsonObject.put(PRODUCT_IDS, productIds);
+
+        // channel json array
+        JSONArray channels = new JSONArray();
+        channels.add(channel.getChannel());
+        jsonObject.put(CHANNELS, channels);
+
+        return jsonObject.toString();
+    }
 }
